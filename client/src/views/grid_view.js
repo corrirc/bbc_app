b73fde65f3ddfe93bbb231b24daf3846e25bab59
anne-other <anne.c.ince@gmail.com>
2018-09-08 14:52:46 +0100

    adding bind events functionto grid view

--- client/src/views/grid_view.js
+++ client/src/views/grid_view.js
@@ -1,4 +1,14 @@
 const PubSub = require('../helpers/pub_sub.js');
 const LandmarkGridItemView = require('./item_view.js');
 
+const LandmarkGridView = function (container) {
+  this.container = container;
+}
+
+LandmarkGridView.prototype.bindEvents = function () {
+  PubSub.subscribe('Landmarks:data-loaded', (event) => {
+    this.render(event.detail);
+  });
+};
+
 module.exports = LandmarkGridView;
