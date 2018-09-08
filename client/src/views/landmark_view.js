4f005a78f7b0d91b84b5c4d6f05ea6deb22b5e65
Robert <robert.deignan@outlook.com>
2018-09-08 11:15:36 +0100

    Adds landmark view

---
+++ client/src/views/landmark_view.js
@@ -0,0 +1,29 @@
+const PubSub = require('../helpers/pub_sub');
+
+const LandmarkView = function(container) {
+  this.container = container;
+}
+
+LandmarkView.prototype.bindEvents() = function() {
+  PubSub.subscribe("LandmarkGridItemView:landmark-selected", landmark => {
+    this.render(landmark);
+  })
+}
+
+LandmarkView.prototype.render = function(landmark) {
+  this.container.innerHTML = '';
+
+  const landmarkName = document.createElement('h2');
+  landmarkName.textContent = landmark.name;
+  this.container.appendChild(landmarkName);
+
+  const landmarkLocation = document.createElement('p');
+  landmarkLocation.textContent = `Location: ${landmark.location}`
+  this.container.appendChild(landmarkLocation);
+
+  const landmarkFacts = document.createElement('p');
+  landmarkFacts.textContent = landmark.facts.
+  this.container.appendChild(landmarkFacts);
+}
+
+module.exports = LandmarkView;
