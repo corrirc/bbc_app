d5586d3a80d08350dbedf1d008d8541082f23064
anne-other <anne.c.ince@gmail.com>
2018-09-08 16:01:51 +0100

    adding the render function to the landmark grid view and creating a div under it

--- client/src/views/item_view.js
+++ client/src/views/item_view.js
@@ -2,4 +2,11 @@ const LandmarkGridItemView = function (container) {
   this.container = container;
 };
 
+LandmarkGridItemView.prototype.render = function (landmark) {
+  const landmarkContainer = document.createElement('div');
+  landmarkContainer.class = 'landmark';
+
+  const landmarkName = this.createHeading(landmark.landmark_name)
+};
+
 module.exports = LandmarkGridItemView;
