aa2088ef1941dbc78be4babb096faa8125f1b379
Shannon Holmes <snholmes13@gmail.com>
2018-09-09 12:43:53 +0100

    add clipart and urls

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -6,95 +6,120 @@ db.landmarks.insertMany([
     landmark_name: "Great Wall of China",
     landmark_location: "China",
     landmark_picture:
-    landmark_facts:
-    clipart:
+    landmark_facts: ""
+    clipart: "/images/clipart/greatwall.jpeg",
+    did_you_know_fact:
+    landmark_url: "https://www.chinahighlights.com/greatwall/"
   },
   {
     landmark_name: "Machu Picchu",
     landmark_location: "Peru",
     landmark_picture:
+    landmark_facts: ""
+    clipart: "/images/clipart/machupicchu.jpeg",
+    did_you_know_fact:
+    landmark_url "https://www.lonelyplanet.com/peru/machu-picchu"
+  },
+  {
+    landmark_name: "CodeClan Glasgow",
+    landmark_location: "Glasgow",
+    landmark_picture:
     landmark_facts:
-    clipart:
+    clipart: ""
+    did_you_know_fact:
+    landmark_url "https://codeclan.com/"
   },
   {
     landmark_name: "The Statue of Liberty",
     landmark_location: "United States of America",
     landmark_picture:
-    landmark_facts:
-    clipart:
+    landmark_facts: ""
+    clipart: "/images/clipart/liberty.jpg",
+    did_you_know_fact:
+    landmark_url: "https://www.nps.gov/stli/index.htm"
   }
 
   {
     landmark_name: "Eiffel Tower",
     landmark_location: "France",
     landmark_picture:
-    landmark_facts:
-    clipart:
+    landmark_facts: ""
+    clipart: "/images/clipart/eiffle.jpeg",
+    did_you_know_fact:
+    landmark_url: "https://www.toureiffel.paris/en"
   },
-
   {
     landmark_name: "Leaning Tower of Pisa",
     landmark_location: "Italy",
     landmark_picture:
-    landmark_facts:
-    clipart:
+    landmark_facts: ""
+    clipart: "/images/clipart/pisa.jpeg",
+    did_you_know_fact:
+    landmark_url: "http://www.towerofpisa.org/"
   },
-
   {
     landmark_name: "Big Ben",
     landmark_location: "United Kingdom",
     landmark_picture:
-    landmark_facts:
-    clipart:
+    landmark_facts: ""
+    clipart: "/images/clipart/ben.jpeg",
+    did_you_know_fact:
+    landmark_url: "https://www.parliament.uk/bigben"
   },
-
   {
     landmark_name: "Taj Mahal",
     landmark_location: "India",
     landmark_picture:
-    landmark_facts:
-    clipart:
+    landmark_facts: ""
+    clipart: "/images/clipart/tajmahal.jpeg",
+    did_you_know_fact:
+    landmark_url: "https://whc.unesco.org/en/list/252"
   },
-
   {
     landmark_name: "Great Pyramid of Giza",
     landmark_location: "Egypt",
     landmark_picture:
-    landmark_facts:
-    clipart:
+    landmark_facts: ""
+    clipart: "/images/clipart/pyramids.jpeg",
+    did_you_know_fact:
+    landmark_url "https://www.nationalgeographic.com/archaeology-and-history/archaeology/giza-pyramids/"
   },
-
   {
-    landmark_name: "Grand Canyon",
-    landmark_location: "USA",
+    landmark_name: "Moai Statues",
+    landmark_location"Easter Island",
     landmark_picture:
-    landmark_facts:
-    clipart:
+    landmark_facts: ""
+    clipart: "/images/clipart/moai.jpeg",
+    did_you_know_fact:
+    landmark_url: "https://www.easterisland.travel/easter-island-facts-and-info/moai-statues/"
   },
-
-  {
-    landmark_name: "The Colosseum",
-    landmark_location"Italy",
-    landmark_picture:
-    landmark_facts:
-    clipart:
-  },
-
   {
     landmark_name: "Acropolis",
     landmark_location: "Greece",
     landmark_picture:
-    landmark_facts:
-    clipart:
+    landmark_facts: ""
+    clipart: "/images/clipart/acropolis.jpeg",
+    did_you_know_fact:
+    landmark_url: "https://whc.unesco.org/en/list/404"
   },
+  {
+    landmark_name: "The Colosseum",
+    landmark_location"Italy",
+    landmark_picture:
+    landmark_facts: ""
+    clipart: "/images/clipart/"
+    did_you_know_fact:
+    landmark_url "https://www.rome.info/colosseum/"
+  },
+  //
   // {
   //   landmark_name: "",
   //   landmark_location: "",
   //   landmark_picture:
   //   landmark_facts:
   //   clipart:
+  //   did_you_know_fact:
+  //   landmark_url:
   // },
 
 ])
-
-// https://www.listchallenges.com/150-most-famous-landmarks-in-the-world
