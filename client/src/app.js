b2f371e31d98f154ff928dacaaf5fc8a6413dd48
Ricky <rickycorrigan@icloud.com>
2018-09-10 09:35:32 +0100

    app.js complete

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,9 @@
-const LandGridView = require(''); //gridview
-const Landmark = require(''); //model
+const LandmarkGridView = require('./views/grid_view'); //gridview
+const Landmark = require('./models/landmark'); //model
 
 document.addEventListener('DOMContentLoaded') , () => {
-  const landmarkContainer = document.querySelector('div#landmarks');
-  const landmarkGridView = new LandGridView(Container);
+  const landmarkContainer = document.querySelector('#landmarkgrid-container');
+  const landmarkGridView = new LandmarkGridView(landmarkContainer);
   landmarkGridView.bindEvents();
 
   const landmark = new Landmark();
