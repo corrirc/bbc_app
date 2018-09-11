100a8cbca7611f08f82d649d1eef51594d35f603
Robert <robert.deignan@outlook.com>
2018-09-11 18:35:09 +0100

    Add country and continent filtering to model

--- client/src/models/landmark.js
+++ client/src/models/landmark.js
@@ -3,6 +3,7 @@ const Request = require('../helpers/request');
 
 const Landmark = function() {
   this.url = 'http://localhost:3000/api/landmarks';
+  this.landmarks = null;
 }
 
 Landmark.prototype.bindEvents = function() {
@@ -15,12 +16,33 @@ Landmark.prototype.bindEvents = function() {
         PubSub.publish("LandmarkModel:landmark-selected", data[0]);
       })
   })
+
   PubSub.subscribe('landmarkGridItemView:toggled', (event) => {
     const toggleId = event.detail.id;
     const payload = event.detail.status;
     request = new Request(`${this.url}`);
     request
     .update(toggleId, payload);
+  })
+
+  PubSub.subscribe('DropdownView:country-selected', event => {
+    const country = event.detail;
+    const request = new Request(`${this.url}/countries/${country}`)
+    request
+      .get()
+      .then(data => {
+        PubSub.publish('Landmarks:data-loaded', data);
+      })
+  })
+
+  PubSub.subscribe('DropdownView:continent-selected', event => {
+    const continent = event.detail;
+    const request = new Request(`${this.url}/continents/${continent}`)
+    request
+      .get()
+      .then(data => {
+        PubSub.publish('Landmarks:data-loaded', data);
+      })
   })
 }
 
@@ -29,10 +51,29 @@ Landmark.prototype.getData = function() {
   request
     .get()
     .then(data => {
-      PubSub.publish("Landmarks:data-loaded", data);
+      PubSub.publish('Landmarks:data-loaded', data);
+      PubSub.publish('LandmarkModel:countries-ready', this.getCountires(data));
+      PubSub.publish('LandmarkModel:continents-ready', this.getContinents(data));
     })
     .catch(console.error);
 }
 
+Landmark.prototype.getCountires = function(landmarks) {
+  const countries = landmarks.map(landmark => {
+    return landmark.landmark_location;
+  })
+  const unique =  countries
+    .filter((country, index, array) => { return array.indexOf(country) === index });
+  return unique;
+}
+
+Landmark.prototype.getContinents = function(landmarks) {
+  const continents = landmarks.map(landmark => {
+    return landmark.continent;
+  })
+  const unique =  continents
+    .filter((continent, index, array) => { return array.indexOf(continent) === index });
+  return unique;
+}
 
 module.exports = Landmark;
